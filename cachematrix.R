7e75e9be70b6325f630ab178017e6c7301c474d2
Rob Michielsen <rob.michielsen@gmail.com>
2015-01-24 08:38:09 +0100

    corrected type

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## The first function, makeVector creates a special "matrix", which is really a list containing a function to
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 ## set the value of the matrix
 ## get the value of the matrix
 ## set the value of the inverse
